08c720ee17c5e85b7b66e8cac680e3fa2b8e4c46
Hilary Lewis <hilarylewis92@gmail.com>
2017-05-23 09:46:57 -0600

    Add firebase auth"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import firebase from 'firebase'
-import { Header } from './components/common'
+import { Header, Button } from './components/common'
 import LoginForm from './components/LoginForm'
 
 class App extends Component {
+  state = { loggedIn: false }
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyBtPoEYOJF21zq3hMySw9BpF8rcwbKARMo',
@@ -14,13 +16,33 @@ class App extends Component {
       storageBucket: 'auth-a92ee.appspot.com',
       messagingSenderId: '691529598350'
     })
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true })
+      } else {
+        this.setState({ loggedIn: false })
+      }
+    })
+  }
+
+  renderContent() {
+    if (this.state.loggedIn) {
+      return (
+        <Button>
+          Log Out
+        </Button>
+      )
+    }
+
+    return <LoginForm />
   }
 
   render() {
     return(
       <View>
         <Header headerText="Authentication"/>
-        <LoginForm />
+        {this.renderContent()}
       </View>
     )
   }
